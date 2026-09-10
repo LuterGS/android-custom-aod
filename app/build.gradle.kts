a445126ba371aea79d693d7882d2f90c6835a79c
Luther Lee <19836058+LuterGS@users.noreply.github.com>
2026-09-10 16:21:49 +0900

    chore: prepare v2.1 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,8 +7,8 @@ android {
         applicationId = "dev.lutergs.sgaod"
         minSdk = 31
         targetSdk = 36
-        versionCode = 4
-        versionName = "2.0"
+        versionCode = 5
+        versionName = "2.1"
         testInstrumentationRunner = "android.test.InstrumentationTestRunner"
     }
     val releaseKeystorePath = System.getenv("RELEASE_KEYSTORE_PATH")
